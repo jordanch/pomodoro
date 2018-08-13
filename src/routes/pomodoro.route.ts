57fb774c600c93e71900b4fded5a0fad0e595fa4
Jordan Cole Hunt <jordancolehunt@gmail.com>
2018-08-13 21:12:59 +0200

    update pomodoro route

--- src/routes/pomodoro.route.ts
+++ src/routes/pomodoro.route.ts
@@ -1,18 +1,19 @@
 import KoaRouter from "koa-router"
-import { start, state } from "../controllers"
+import { start, state, stop } from "../controllers"
 import {
   IPomoStartCtrl200,
   IPomoStateCtrl200,
-  IPomoStateCtrl40x
+  IPomoStateCtrl40x,
+  IPomoStopCtrl200,
+  IPomoStopCtrl40x
 } from "../models"
 
 const router = new KoaRouter()
 
-/**
- * Start a Pomodoro timer.
- * @returns {Pomodoro} Pomodoro instance created.
- */
 router
+  /**
+   * Start a Pomodoro timer.
+   */
   .get("/start", async (ctx) => {
     const response = await start()
     if ((<IPomoStartCtrl200>response).status === 200) {
@@ -20,6 +21,9 @@ router
       ctx.body = response.data
     }
   })
+  /**
+   * Query Pomodoro state by id..
+   */
   .get("/state", (ctx) => {
     const response = state(ctx)
 
@@ -40,6 +44,31 @@ router
       console.log("Serving 404 NOT FOUND")
       ctx.status = (<IPomoStateCtrl40x>response).status
       ctx.body = (<IPomoStateCtrl40x>response).data
+    }
+  })
+  /**
+   * Stop a pomodoro by id.
+   */
+  .get("/stop", (ctx) => {
+    const response = stop(ctx)
+
+    if (response.status === 200) {
+      console.log("Serving 200 OK")
+      // type assertion going on heeya.
+      ctx.status = (<IPomoStopCtrl200>response).status
+      ctx.body = (<IPomoStopCtrl200>response).data
+    }
+
+    if (response.status === 400) {
+      console.log("Serving 400 BAD REQUEST")
+      ctx.status = (<IPomoStopCtrl40x>response).status
+      ctx.body = (<IPomoStopCtrl40x>response).data
+    }
+
+    if (response.status === 404) {
+      console.log("Serving 404 NOT FOUND")
+      ctx.status = (<IPomoStopCtrl40x>response).status
+      ctx.body = (<IPomoStopCtrl40x>response).data
     }
   })
 
