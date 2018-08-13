366d599c8a59532fa3eab5f42d618b58c7dcf9ac
Jordan Cole Hunt <jordancolehunt@gmail.com>
2018-08-13 21:12:14 +0200

    pomodoro controller update

--- src/controllers/pomodoro.controller.ts
+++ src/controllers/pomodoro.controller.ts
@@ -3,11 +3,17 @@ import {
   ISessionCache,
   IPomoStartCtrl200,
   IPomoStateCtrl200,
-  IPomoStateCtrl40x
+  IPomoStateCtrl40x,
+  IStopRequest,
+  IPomoStopCtrl40x,
+  IPomoStopCtrl200
 } from "../models"
 import KoaRouter from "koa-router"
 import { IStateRequest } from "../models"
 
+/**
+ * Start a pomodoro timer.
+ */
 export async function start() {
   const id = await getNextId(cache)
   const pomodoro = new Pomodoro(id)
@@ -19,10 +25,6 @@ export async function start() {
 
 /**
  * Get the state of a pomodoro by id.
- *
- * @export
- * @param {KoaRouter.IRouterContext} ctx - request needs `id` query string param.
- * @returns
  */
 export function state(ctx: KoaRouter.IRouterContext) {
   const { id } = ctx.query as IStateRequest
@@ -60,10 +62,43 @@ export function state(ctx: KoaRouter.IRouterContext) {
 }
 
 /**
+ * Stop pomodoro timer.
+ */
+export function stop(
+  ctx: KoaRouter.IRouterContext
+): IPomoStopCtrl200 | IPomoStopCtrl40x {
+  const { id } = ctx.query as IStopRequest
+
+  // no id in request
+  if (!id) {
+    console.log("No id present on request")
+    return {
+      status: 400,
+      data: "Pass in a querystring param 'id=<number>'"
+    } as IPomoStopCtrl40x
+  }
+
+  const pomodoro = cache.sessions.byId[id]
+
+  // found using id
+  if (pomodoro) {
+    console.log(`Stopping pomodoro id ${id}`)
+
+    pomodoro.stop()
+
+    return {
+      status: 200,
+      data: `Stopped ${id}`
+    } as IPomoStopCtrl200
+  }
+
+  // not found using id
+  console.log("Could not find pomodoro by id.")
+  return { status: 404, data: `Could not find id ${id}` } as IPomoStopCtrl40x
+}
+
+/**
  * Get the next pomodoro's id. Async operation. todo: add db query.
- *
- * @param {ISessionCache} storage
- * @returns {number}
  */
 function getNextId(storage: ISessionCache): number {
   const allIds = storage.sessions.allIds.slice()
